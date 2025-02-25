10102cd41952bf54c523d07ee18202ea70ec89c5
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2025-02-25 11:47:18 +0100

    finish wall slide

--- code/player.py
+++ code/player.py
@@ -1,4 +1,5 @@
 from settings import *
+from timer import Timer
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, collision_sprites):
@@ -21,19 +22,25 @@ class Player(pygame.sprite.Sprite):
         self.collision_sprites = collision_sprites
         self.on_surface = {'floor': False, 'left': False, 'right': False}
         
-        self.display_surface = pygame.display.get_surface()
+        # timer
+        self.timers = {
+            'wall jump': Timer(400),
+            'wall slide block': Timer(250)
+        }
         
     def input(self):
         keys = pygame.key.get_pressed()
         input_vector = vector(0,0)
-        if keys[pygame.K_RIGHT]:
-            input_vector.x += 1
-        if keys[pygame.K_LEFT]:
-            input_vector.x -= 1
-        self.direction.x = input_vector.normalize().x if input_vector else input_vector.x
+        if not self.timers['wall jump'].active:
+            if keys[pygame.K_RIGHT]:
+                input_vector.x += 1
+            if keys[pygame.K_LEFT]:
+                input_vector.x -= 1
+            self.direction.x = input_vector.normalize().x if input_vector else input_vector.x
 
         if keys[pygame.K_SPACE]:
             self.jump = True
+            self.timers['wall jump'].activate()
     
     def move(self, dt):
         # horizontal
@@ -41,7 +48,7 @@ class Player(pygame.sprite.Sprite):
         self.collision('horizontal')
         
         # vertical
-        if not self.on_surface['floor'] and any((self.on_surface['left'], self.on_surface['right'])):
+        if not self.on_surface['floor'] and any((self.on_surface['left'], self.on_surface['right'])) and not self.timers['wall slide block'].active:
             self.direction.y = 0
             self.rect.y += self.gravity / 10 * dt
         else:
@@ -52,13 +59,14 @@ class Player(pygame.sprite.Sprite):
         if self.jump:
             if self.on_surface['floor']:
                 self.direction.y = -self.jump_height
-            elif any((self.on_surface['left'], self.on_surface['right'])):
+                self.timers['wall slide block'].activate()
+            elif any((self.on_surface['left'], self.on_surface['right'])) and not self.timers['wall slide block'].active:
+                self.timers['wall jump'].activate()
                 self.direction.y = -self.jump_height
                 self.direction.x = 1 if self.on_surface['left'] else -1
             self.jump = False
                 
-        self.collision('vertical')
-        
+        self.collision('vertical')   
         
     def check_contact(self):
         floor_rect = pygame.Rect(self.rect.bottomleft, (self.rect.width,2))
@@ -92,8 +100,13 @@ class Player(pygame.sprite.Sprite):
                         self.rect.bottom = sprite.rect.top
                     self.direction.y = 0
     
+    def update_timers(self):
+        for timer in self.timers.values():
+            timer.update()
+    
     def update(self, dt):
         self.old_rect = self.rect.copy()
+        self.update_timers()
         self.input()
         self.move(dt)
         self.check_contact()
