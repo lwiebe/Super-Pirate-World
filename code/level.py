645b436430068638b36dd4c77ae13a6ff9334074
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2025-02-25 12:44:18 +0100

    init moving platforms

--- code/level.py
+++ code/level.py
@@ -1,5 +1,5 @@
 from settings import *
-from sprites import Sprite
+from sprites import Sprite, MovingSprite
 from player import Player
 
 class Level:
@@ -13,14 +13,29 @@ class Level:
         self.setup(tmx_map)
         
     def setup(self, tmx_map):
+        # tiles
         for x, y, surf in tmx_map.get_layer_by_name('Terrain').tiles():
             Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, (self.all_sprites, self.collision_sprites)) 
-            
+        
+        # objects
         for obj in tmx_map.get_layer_by_name('Objects'):
             if obj.name == 'player':
                 Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
-            
         
+        # moving objects
+        for obj in tmx_map.get_layer_by_name('Moving Objects'):
+            if obj.name == 'helicopter':
+                if obj.width > obj.height: # horizontal
+                    move_dir = 'x'
+                    start_pos = (obj.x, obj.y + obj.height / 2)
+                    end_pos = (obj.x + obj.width, obj.y + obj.height / 2)
+                else: # vertical
+                    move_dir = 'y'
+                    start_pos = (obj.x + obj.width / 2, obj.y)
+                    end_pos = (obj.x + obj.width / 2, obj.y + obj.height)
+                speed = obj.properties['speed']
+                MovingSprite((self.all_sprites, self.collision_sprites), start_pos, end_pos, move_dir, speed)
+            
     def run(self, dt):
         self.display_surface.fill('black')
         self.all_sprites.update(dt)
